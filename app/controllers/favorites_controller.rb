007ab15c0a0f7a52e901a2c2ef7702f35f65ab5b
ryuto0111 <129473003+ryuto0111@users.noreply.github.com>
2023-05-20 06:16:14 +0000

    [Finish] 1-6

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,16 +1,17 @@
 class FavoritesController < ApplicationController
-  before_action :authenticate_user!
 
   def create
-    @book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: @book.id)
-    favorite.save
+    book = Book.find(params[:book_id])
+    @favorite = current_user.favorites.new(book_id: book.id)
+    @favorite.save
+    render 'replace_btn'
   end
 
   def destroy
-    @book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: @book.id)
-    favorite.destroy
+    book = Book.find(params[:book_id])
+    @favorite = current_user.favorites.find_by(book_id: book.id)
+    @favorite.destroy
+    render 'replace_btn'
   end
 
 end
