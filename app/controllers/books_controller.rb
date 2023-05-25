144ac2e0192defeb7322869a6154e27f135613be
ryuto0111 <129473003+ryuto0111@users.noreply.github.com>
2023-05-25 08:47:14 +0000

    [Finish]Theme9a

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,8 +7,6 @@ class BooksController < ApplicationController
     unless ViewCount.find_by(user_id: current_user.id, book_id: @book.id)
       current_user.view_counts.create(book_id: @book.id)
     end
-    @user = @book.user
-    @new_book = Book.new
     @book_comment = BookComment.new
   end
 
