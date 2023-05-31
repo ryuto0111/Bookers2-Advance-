7c6f8b84de2d8633d5bf16644616d75c021d0ee3
ryuto0111 <129473003+ryuto0111@users.noreply.github.com>
2023-05-31 09:51:54 +0000

    [Add]指定日の投稿数検索機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,14 +28,7 @@ class UsersController < ApplicationController
 
   def search
     @user = User.find(params[:user_id])
-    @books = @user.books
-    @book = Book.new
-    if params[:created_at] == ""
-      @search_book = "日付を選択してください"
-    else
-      create_at = params[:created_at]
-      @search_book = @books.where(['created_at LIKE ? ', "#{create_at}%"]).count
-    end
+    @books = @user.books.where(created_at: params[:created_at].to_date.all_day)
   end
 
   private
